b7babd278e2e317f6970cf11ff612c9cd9905092
Ave-Wat <wattsa2@carleton.edu>
2021-07-06 17:06:42 -0500

    fixed issues and added fade-in

--- webpage.js
+++ webpage.js
@@ -12,11 +12,28 @@ function mobileCloseNav() {
   closeButton.style.display = "none";
 }
 
+function fadeIn() {
+  for (var i = 0; i < contentArray.length; i ++){
+    var contentBox = contentArray[i]
+    var distInView = contentBox.getBoundingClientRect().top - window.innerHeight + 20;
+    if (distInView < 0) {
+      contentBox.classList.add("inView");
+    } else {
+      contentBox.classList.remove("inView");
+    }
+  }
+}
+
 function initialize() {
   var mobileOpenNavButton = document.getElementById('open_icon');
   mobileOpenNavButton.onclick = mobileOpenNav;
   var mobileCloseNavButton = document.getElementById('close_icon');
   mobileCloseNavButton.onclick = mobileCloseNav;
+  contentArray = document.querySelectorAll(".content-container");
+  console.log(contentArray);
+  window.addEventListener('scroll', fadeIn);
+  fadeIn();
 }
 
+let contentArray;
 window.onload = initialize;
